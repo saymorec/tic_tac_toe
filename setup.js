b854c75e30006b68f84f01f08ca99eb207d96d2f
Saymore Chifamba <saymore.chifamba@prodigyfinance.com>
2024-06-01 02:25:47 +0200

    add setup js file

---
+++ setup.js
@@ -0,0 +1,10 @@
+// Import necessary libraries
+const { chromium } = require('playwright');
+
+// Configure test environment
+let browser;
+
+// Export additional variables or setup functions if needed
+module.exports = {
+  browser
+};
